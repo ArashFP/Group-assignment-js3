48e4141c2b264dd340ca86296881503de997fa03
Janine Escobillo <janineescobillo@hotmail.com>
2024-05-15 19:41:12 +0200

    changes bg and font

--- event-manager/app/admin/_components/header.jsx
+++ event-manager/app/admin/_components/header.jsx
@@ -9,12 +9,12 @@ import { auth } from "@/firebase/config"
 const Header = () => {
   return (
     <nav>
-    <ul className="flex justify-between items-center bg-blue-500 py-8 px-4">
-        <Link href="/" className="font-semibold text-white ">Event-Maker</Link>
+    <ul className="flex justify-between items-center bg-slate-800 py-8 px-4">
+        <Link href="/" className="font-semibold font-mono text-white ">Event-Maker</Link>
         <div className="flex gap-4 items-center">
         <li className="flex justify-center items-center">
-            <Link className="text-white px-4 font-semibold hover:text-blue-800" href="/admin">Event List</Link>
-            <Link  className="text-white px-4 font-semibold hover:text-blue-800" href="/admin/events/create-event">Create Event</Link>
+            <Link className="text-white px-4 font-semibold font-mono hover:text-blue-800" href="/admin">Event List</Link>
+            <Link  className="text-white px-4 font-semibold font-mono hover:text-blue-800" href="/admin/events/create-event">Create Event</Link>
             <Button variant="outlineWhite" className="bg-transparent border-white text-white font-semibold hover:text-white hover:bg-blue-800 rounded-full" onClick={async () => {
               await signOut(auth)
             }} >Sign out</Button>
--- event-manager/app/page.jsx
+++ event-manager/app/page.jsx
@@ -8,7 +8,7 @@ function LandingPage() {
   return (
   <>
       <main className="h-screen flex flex-col items-center justify-center text-center ">
-        <h1 className="lg:text-6xl font-bold sm:text-4xl">Welcome to Event-Maker</h1>
+        <h1 className="lg:text-6xl font-bold font-mono sm:text-4xl">Welcome to Event-Maker</h1>
         <p className="lg:text-lg  sm:text-sm  mt-4 text-muted-foreground">Create and join events</p>
           <div className="flex gap-4 mt-20">
             <Button asChild className="bg-transparent text-current w-[180px] h-12 rounded-full font-semibold hover:bg-blue-500 border-2 border-blue-500">

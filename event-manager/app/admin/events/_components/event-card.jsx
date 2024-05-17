d7a7b0ec83bc193a343ef75b1897c82d5c66d0eb
Yousha003 <126115684+Yousha003@users.noreply.github.com>
2024-05-17 15:17:16 +0200

    lägger till delteEvent och knapp till denna

--- event-manager/app/admin/events/_components/event-card.jsx
+++ event-manager/app/admin/events/_components/event-card.jsx
@@ -1,23 +1,28 @@
-import Image from "next/image"
-import Link from "next/link"
+import Image from "next/image";
+import Link from "next/link";
+import { db } from "@/firebase/config";
 
-const EventCard = ({ id, name, price, imageURL}) => {
+const EventCard = ({ id, name, price, imageURL }) => {
+  
   return (
-    <Link href={`/events/${id}`} className="">
-      <div className="h-full aspect-video">
-        <Image 
-          src={imageURL || "/image-placeholder.png"} 
-          alt={name}
-          width={100}
-          height={100}
-          className="w-full h-full object-cover"
-        />
-      </div>
-      <div className="flex pr-10 gap-10">
-        <p className="text-xl font-semibold">{name}</p>
-        <p>{price}</p>
-      </div>
-    </Link>
-  )
-}
-export default EventCard
+    <div className="relative">
+      <Link href={`/events/${id}`} className="">
+      
+        <div className="h-full aspect-video">
+          <Image
+            src={imageURL || "/image-placeholder.png"}
+            alt={name}
+            width={100}
+            height={100}
+            className="w-full h-full object-cover"
+          />
+        </div>
+        <div className="flex pr-10 gap-10">
+          <p className="text-xl font-semibold">{name}</p>
+          <p>{price}</p>
+        </div>
+      </Link>
+    </div>
+  );
+};
+export default EventCard;
--- event-manager/app/admin/page.jsx
+++ event-manager/app/admin/page.jsx
@@ -2,11 +2,22 @@
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-
+import { db } from "@/firebase/config";
+import { doc, deleteDoc } from "firebase/firestore";
 
 function AdminPage() {
   const [events, setEvents] = useState([]);
   const [isLoading, setLoading] = useState(true);
+
+  const deleteEvent = async (id) => {
+    try {
+      await deleteDoc(doc(db, "events", id));
+      console.log(`Event with id ${id} deleted successfully`);
+      fetchEvents();
+    } catch (error) {
+      console.error('Error deleting event: ', error);
+    }
+  };
 
   const fetchEvents = async () => {
     try {
@@ -21,6 +32,7 @@ function AdminPage() {
       });
       setEvents(eventData);
       setLoading(false);
+      console.log(eventData);
     } catch (err) {
       console.log("Error", err);
       setLoading(false);
@@ -28,33 +40,50 @@ function AdminPage() {
     }
   };
 
+
   useEffect(() => {
     fetchEvents();
   }, []);
 
   return (
     <div className="container mx-auto px-4">
-      <h2 className="text-6xl font-bold text-center font-mono py-14">Event List</h2>
+      <h2 className="text-6xl font-bold text-center font-mono py-14">
+        Event List
+      </h2>
       <div className="grid grid-cols-3 gap-10">
-        {events.map((event) => ( 
-          <Link
-            key={event.id}
-            href={`/admin/events/${event.id}`}
-            className="p-6 rounded-lg border-2 shadow-lg cursor-pointer pb flex flex-col gap-4"
-          >
-            <img src={event.imageURL} alt={event.eventName} className="object-cover max-h-52 max-w-60 rounded" /> 
-            <h1 className="font-bold text-2xl">{event.eventName}</h1>
-            <p className="text-sm text-muted-foreground flex items-center gap-2">
-              <span>{event.eventDate}</span>
-            </p>
-            <p className="text-sm text-muted-foreground flex items-center gap-2">
-              <span>{event.eventPrice}</span>
-            </p>
+        {events.map((event) => (
+            <div key={event.id}>
 
-            <p className="text-sm text-muted-foreground flex items-center gap-2">
-              {event.eventLocation}
-            </p>
-          </Link>
+            <Link
+              key={event.id}
+              href={`/admin/events/${event.id}`}
+              className="p-6 rounded-lg border-2 shadow-lg cursor-pointer pb flex flex-col gap-4"
+            >
+              <img
+                src={event.imageURL}
+                alt={event.eventName}
+                className="object-cover max-h-52 max-w-60 rounded"
+              />
+              <h1 className="font-bold text-2xl">{event.eventName}</h1>
+              <p className="text-sm text-muted-foreground flex items-center gap-2">
+                <span>{event.eventDate}</span>
+              </p>
+              <p className="text-sm text-muted-foreground flex items-center gap-2">
+                <span>{event.eventPrice}</span>
+              </p>
+
+              <p className="text-sm text-muted-foreground flex items-center gap-2">
+                {event.eventLocation}
+              </p>
+            </Link>
+              
+            <button
+              onClick={ () => deleteEvent(event.id)}
+              className=" bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Delete Event
+            </button>
+          </div>
         ))}
       </div>
     </div>

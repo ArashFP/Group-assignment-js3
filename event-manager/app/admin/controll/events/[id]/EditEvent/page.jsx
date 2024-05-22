c617819934952b7e1de73d2078afe5d526b20113
Arash Parsa <arash_afp_94@hotmail.com>
2024-05-22 19:24:33 +0200

    updates admin detail event page, adds EditEvent btn + routing

---
+++ event-manager/app/admin/controll/events/[id]/EditEvent/page.jsx
@@ -0,0 +1,5 @@
+export default function EditEventPage() {
+  return (
+    <div>EditEventPage</div>
+  )
+}
--- event-manager/app/admin/controll/events/[id]/page.jsx
+++ event-manager/app/admin/controll/events/[id]/page.jsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button"
 import getDocument from "@/lib/getDocument"
+import Link from "next/link"
 
 
 export default async function DetailEventPage({ params }) {
@@ -9,16 +10,21 @@ export default async function DetailEventPage({ params }) {
 
   return (
     <div className="flex flex-col h-screen justify-center items-center">
-      <div className="bg-slate-800 px-5 py-5 rounded-2xl">
-        <h1  className="text-4xl text-center mb-3 ">{event?.eventName}</h1>
+      <div className="bg-slate-800 px-5 py-5 rounded-2xl w-1/3 flex flex-col ">
+        <h1  className="text-4xl text-center mb-8 ">{event?.eventName}</h1>
         <p className="mb-1">Date: {event?.eventDate?.toDate().toLocaleDateString('en-GB')}</p>
         <p className="mb-1">Description: {event?.eventDescription}</p>
         <p className="mb-1">Location: {event?.eventLocation}</p>
         <p className="mb-1">Price: {event?.eventPrice}</p>
         <p className="mb-5">Tickets remaining: {event?.eventQuantity}</p>
-        <img src={event?.imageURL} alt={event?.eventName}  className="rounded-3xl"/>
-        <Button 
-        className="w-full mt-5 bg-slate-100/50 transform transition-transform duration-200 hover:scale-105 active:bg-green-500">Book Ticket!</Button>
+        <img src={event?.imageURL} alt={event?.eventName} className="rounded-3xl w-full object-cover mx-auto max-w-52"/>
+
+        <Link href={`/admin/controll/events/${params.id}/EditEvent`} as={`/admin/controll/events/${params.id}/EditEvent`}>
+          <Button 
+            className="w-full mt-5 bg-slate-100/50 transform transition-transform duration-200 hover:scale-105 active:bg-green-500">
+              Edit Event
+          </Button>
+        </Link> 
       </div>
   </div>
   )

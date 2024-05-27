3650c5f9a338848577c4764fb5411b4c49e62f42
Janine Escobillo <janineescobillo@hotmail.com>
2024-05-27 19:27:38 +0200

    adds a component name user-list-item

---
+++ event-manager/app/admin/controll/upgrade-users/_components/user-list-item.jsx
@@ -0,0 +1,19 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { cn } from "@/lib/utils"
+import { Trash2 } from "lucide-react"
+
+
+export const UserListItem = ({email, imageUrl, onClick, isSelected}) => {
+  return (
+    <div onClick={onClick} className={cn("flex items-center justify-between p-2 hover: bg-slate-500/10 cursor-pointer",
+      isSelected && "hover: bg-slate-500/55"
+    )}>
+      <div className="flex items-center gap-4">
+        <Avatar className="size-8">
+          <AvatarImage src={imageUrl} />
+        </Avatar>
+        <p>{email}</p>  
+      </div>
+    </div>
+  )
+} 

b9691ad3b161ec3b1f7f983e070f03e3057ae194
Peter <peterblomkvist@hotmail.com>
2024-05-28 14:20:52 +0200

    added fullscreen loader and tweaked landingpage

--- event-manager/app/page.jsx
+++ event-manager/app/page.jsx
@@ -1,61 +1,42 @@
 'use client';
 
+import { FullscreenLoader } from "@/components/fullscreen-loader";
 import { Button } from "@/components/ui/button";
 import { db } from "@/firebase/config";
 import { doc, getDoc } from "firebase/firestore";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
+
 function LandingPage() {
-  const [content, setContent] = useState({ title: '', description: '' });
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
+
+  const getDocumentById = async (collectionName, docId) => {
+  const docRef = doc(db, collectionName, docId)
+  const docSnap = await getDoc(docRef)
+
+  if(docSnap.exists()) {
+    return { id: docSnap.id, ...docSnap.data() }
+  } else {
+    return null
+  }
+}
+
+const [pageData, setPageData] = useState(null)
 
   useEffect(() => {
-    const fetchContent = async () => {
-      try {
-        console.log("Fetching document...");
-        const docRef = doc(db, 'landingPageContent', 'SmtSJZhsEtgOrXbR2FDS');
-        const docSnap = await getDoc(docRef);
-        if (docSnap.exists()) {
-          const data = docSnap.data();
-          console.log("Document data:", data);
-          setContent(data);
-        } else {
-          console.error("Document does not exist");
-          setError('Document does not exist');
-        }
-      } catch (error) {
-        console.error("Error fetching document:", error);
-        setError('Failed to fetch content');
-      } finally {
-        setIsLoading(false);
-      }
-    };
+    const getData = async () => {
+      const data = await getDocumentById('landingPageContent', 'SmtSJZhsEtgOrXbR2FDS')
+      setPageData(data)
+    }
+    getData()
+  }, [])
 
-    fetchContent();
-  }, []);
-
-  if (isLoading) {
-    return (
-      <main className="h-screen flex items-center justify-center">
-        <p>Loading...</p>
-      </main>
-    );
-  }
-
-  if (error) {
-    return (
-      <main className="h-screen flex items-center justify-center">
-        <p>{error}</p>
-      </main>
-    );
-  }
+  if(!pageData) return <FullscreenLoader />
 
   return (
     <main className="h-screen flex flex-col items-center justify-center text-center">
-      <h1 className="lg:text-6xl font-bold font-mono sm:text-4xl">{content.title}</h1>
-      <p className="lg:text-lg sm:text-sm mt-4 text-muted-foreground">{content.description}</p>
+      <h1 className="lg:text-6xl font-bold font-mono sm:text-4xl">{pageData.title}</h1>
+      <p className="lg:text-lg sm:text-sm mt-4 text-muted-foreground">{pageData.description}</p>
       <div className="flex gap-4 mt-20">
         <Button asChild className="bg-transparent text-current w-[180px] h-12 rounded-full font-semibold hover:bg-blue-500 border-2 border-blue-500">
           <Link href="/sign-up">Sign up</Link>

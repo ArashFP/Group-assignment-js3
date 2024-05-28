2fc298c1dab439e704199e8e374677855ffda7ce
Arash Parsa <arash_afp_94@hotmail.com>
2024-05-28 13:03:01 +0200

    adds editEventPage

--- event-manager/app/admin/controll/events/[id]/EditEvent/page.jsx
+++ event-manager/app/admin/controll/events/[id]/EditEvent/page.jsx
@@ -1,5 +1,37 @@
-export default function EditEventPage() {
+'use client'
+
+import { getDocumentById } from "@/lib/getDocument"
+import { useEffect, useState } from "react"
+import EditEventForm from "./_component/edit-event-form"
+
+export default function EditEventPage({ params }) {
+
+  const [event, setEvent] = useState(null)
+
+  useEffect (() => {
+    const getEvent = async () => {
+      const event = await getDocumentById('events', params.id)
+      setEvent(event)
+    }
+    getEvent()
+  } , [])
+
+  if (!event) return null 
+
   return (
-    <div>EditEventPage</div>
+    <div className="flex h-screen justify-start items-start m-10 gap-10">
+      <div className="bg-slate-800 px-5 py-5 rounded-2xl w-1/3 flex flex-col">
+        <h1  className="text-4xl text-center mb-8 ">{event?.eventName}</h1>
+        <p className="mb-1">Date: {event?.eventDate?.toDate().toLocaleDateString('en-GB')}</p>
+        <p className="mb-1">Description: {event?.eventDescription}</p>
+        <p className="mb-1">Location: {event?.eventLocation}</p>
+        <p className="mb-1">Price: {event?.eventPrice}</p>
+        <p className="mb-5">Tickets remaining: {event?.eventQuantity}</p>
+        <img src={event?.imageURL} alt={event?.eventName} className="rounded-3xl w-full object-cover mx-auto max-w-52"/>
+      </div>
+      <div className="bg-slate-800 px-5 py-5 rounded-2xl w-full flex flex-col">
+        <EditEventForm event={event}/>
+      </div>
+    </div>
   )
 }

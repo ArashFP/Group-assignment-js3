21cc7c00e51023a079036352c526390e0c06fa7a
Peter <peterblomkvist@hotmail.com>
2024-05-29 14:01:53 +0200

    added PATCH to cors

--- event-manager/middleware.js
+++ event-manager/middleware.js
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 const allowedOrigins = ['*'];
 
 const corsOptions = {
-  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
   'Access-Control-Allow-Headers': 'Content-Type, Authorization',
 };
 
